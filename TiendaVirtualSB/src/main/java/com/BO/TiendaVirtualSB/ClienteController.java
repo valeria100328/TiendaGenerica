1e6cb6733fb427a9c8aa8b2e25808da21454b5ba
santi0930 <90780943+santi0930@users.noreply.github.com>
2021-10-15 10:25:13 -0500

    Update ClienteController.java

--- TiendaVirtualSB/src/main/java/com/BO/TiendaVirtualSB/ClienteController.java
+++ TiendaVirtualSB/src/main/java/com/BO/TiendaVirtualSB/ClienteController.java
@@ -9,6 +9,11 @@ import org.springframework.boot.json.JsonParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.DAO.TiendaVirtualSB.ClienteDAO;
+import com.DAO.TiendaVirtualSB.ListasDAO;
+import com.DAO.TiendaVirtualSB.ProductosDAO;
+import com.DAO.TiendaVirtualSB.ProveedoresDAO;
+import com.DAO.TiendaVirtualSB.UsuarioDAO;
+import com.DAO.TiendaVirtualSB.VentasDAO;
 import com.DTO.TiendaVirtualSB.ClienteVO;
 import com.fasterxml.jackson.databind.JsonNode;
 
@@ -21,34 +26,34 @@ public class ClienteController {
 	@RequestMapping("/registrarUsuario")
 	public void registrarPersona(ClienteVO persona) 
 	 {
-		ClienteDAO Dao=new ClienteDAO(); 
+		UsuarioDAO Dao=new UsuarioDAO(); 
 	    Dao.registrarUsuario(persona);
 	 }
 	
 	@RequestMapping("/consultarUsuario")   
 	public ClienteVO consultarusuario(int cedula_usuario)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		UsuarioDAO Dao=new UsuarioDAO(); 
 	    return Dao.Consultarusuario(cedula_usuario);
 	}
 	
 	@RequestMapping("/modificarUsuario")   
 	public void modificarUsuario(ClienteVO persona)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		UsuarioDAO Dao=new UsuarioDAO();  
 	    Dao.modificarUsuario(persona);
 	}
 	
 	@RequestMapping("/eliminarUsuario")   
 	public void eliminarUsuario(ClienteVO persona)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		UsuarioDAO Dao=new UsuarioDAO(); 
 	    Dao.eliminarUsuario(persona);
 	}
 	
 	@RequestMapping("/validar")
     public boolean validate(ClienteVO persona) {
-        ClienteDAO Dao=new ClienteDAO(); 
+		UsuarioDAO Dao=new UsuarioDAO(); 
         boolean validar=false;
         try {
 			validar=Dao.validate(persona);
@@ -91,7 +96,7 @@ public class ClienteController {
 	@RequestMapping("/registrarProveedor")
 	public void registrarproveedor(ClienteVO persona) 
 	 {
-		ClienteDAO Dao=new ClienteDAO(); 
+		ProveedoresDAO Dao=new ProveedoresDAO(); 
 	    Dao.registrarProveedor(persona);
 	    
 	 }
@@ -99,56 +104,94 @@ public class ClienteController {
 	@RequestMapping("/consultarProveedor")   
 	public ClienteVO consultarproveedor(long nitproveedor)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		ProveedoresDAO Dao=new ProveedoresDAO(); 
 	    return Dao.ConsultarProveedor(nitproveedor);
 	}
 	
 	@RequestMapping("/modificarProveedor")   
 	public void modificarproveedor(ClienteVO persona)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		ProveedoresDAO Dao=new ProveedoresDAO(); 
 	    Dao.modificarProveedor(persona);
 	}
 	
 	@RequestMapping("/eliminarProveedor")   
 	public void eliminarproveedor(ClienteVO persona)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		ProveedoresDAO Dao=new ProveedoresDAO(); 
 	    Dao.eliminarProveedor(persona);
 	}
 	
 	@RequestMapping("/enlistarproductos")   
 	public void EnlistarProductos(ClienteVO persona)
 	{
-		ClienteDAO Dao=new ClienteDAO();
+		ProductosDAO Dao=new ProductosDAO();
 		Dao.ListarProductos(persona);
 	}
 	
 	@RequestMapping("/Consultarclienteventa") 
 	public ClienteVO ConsultarClienteVenta(int cedula_cliente)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		VentasDAO Dao=new VentasDAO(); 
 	    return Dao.ConsultarClienteVenta(cedula_cliente);
 	}
 	
 	@RequestMapping("/consultarProducto")   
 	public ClienteVO consultarproducto(int codigo_producto)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
-	    return Dao.ConsultarProducto(codigo_producto);
+		VentasDAO Dao=new VentasDAO(); 
+		return Dao.ConsultarProducto(codigo_producto);
 	}
 	
 	@RequestMapping("/ConfirmarVenta")   
 	public void confirmarventa(ClienteVO persona)
 	{
-		ClienteDAO Dao=new ClienteDAO(); 
+		VentasDAO Dao=new VentasDAO(); 
 	    Dao.Confirmarventa(persona);
 	}
 	
 	@RequestMapping("/Creardetalle") 
 	public void Creardetalle(ClienteVO persona) {
-		ClienteDAO Dao=new ClienteDAO(); 
+		VentasDAO Dao=new VentasDAO(); 
 	    Dao.Creardetalle(persona);
+	}
+	
+	@RequestMapping("/total") 
+	public void total(ClienteVO persona) {
+		ListasDAO Dao=new ListasDAO();
+		Dao.total(persona);
+	}
+	
+	@RequestMapping("/eliminartodoproducto")   
+	public void eliminartodoproducto(ClienteVO persona)
+	{
+		ProductosDAO Dao=new ProductosDAO();
+	    Dao.Eliminartodoproducto(persona);
+	}
+	
+	@RequestMapping("/ListarUsuarios")
+	public ArrayList< ClienteVO> listaDePersonas() {
+		ListasDAO Dao=new ListasDAO(); 
+		return Dao.listarUsuarios();	   
+	}
+	
+	@RequestMapping("/ListarClientes")
+	public ArrayList< ClienteVO> listaDeClientes() {
+		ListasDAO Dao=new ListasDAO();
+		return Dao.listarClientes();	   
+	}
+	
+	@RequestMapping("/ListarVentas")
+	public ArrayList< ClienteVO> listaDeVentas() {
+		ListasDAO Dao=new ListasDAO();
+		return Dao.listarVentas();	   
+	}
+	
+	@RequestMapping("/registrarproducto")   
+	public void registrarproductos(ClienteVO persona) 
+	{
+		ProductosDAO Dao=new ProductosDAO();
+	    Dao.registrarProductos(persona);
 	}
 }
 	
